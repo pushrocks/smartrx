cbbb31e0e714cd1bda12c8f9bd30fe43c30ad0f8
Phil Kunz <github@philkunz.com>
2018-12-11 00:47:46 +0100

    fix(core): update

--- ts/smartrx.plugins.ts
+++ ts/smartrx.plugins.ts
@@ -1,7 +1,7 @@
 import * as events from 'events';
 import * as lik from '@pushrocks/lik';
 import * as rxjs from 'rxjs';
-import * as smartevent from 'smartevent';
+import * as smartevent from '@pushrocks/smartevent';
 import * as smartpromise from '@pushrocks/smartpromise';
 
 export { events, lik, rxjs, smartevent, smartpromise };
