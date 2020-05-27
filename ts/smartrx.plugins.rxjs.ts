f9d8175c8caf5b56fc3c11ba44a752a965d34c13
Phil Kunz <gitlab@philkunz.com>
2020-05-27 18:49:44 +0000

    fix(core): update

---
+++ ts/smartrx.plugins.rxjs.ts
@@ -0,0 +1,7 @@
+// this file is intended to keep the bundle size down
+
+export {
+  Observable,
+  Subject,
+  fromEvent
+} from 'rxjs';
--- ts/smartrx.plugins.ts
+++ ts/smartrx.plugins.ts
@@ -1,5 +1,5 @@
 import * as lik from '@pushrocks/lik';
-import * as rxjs from 'rxjs';
+import * as rxjs from './smartrx.plugins.rxjs';
 import * as smartpromise from '@pushrocks/smartpromise';
 
 export { lik, rxjs, smartpromise };
